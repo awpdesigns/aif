500be5b3061bba2be80f4ecdbb9fe7612b44efc6
awpdesigns <awpdesigns56@gmail.com>
2023-09-01 08:16:37 +0700

    modified:   sw.js

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open('aif').then(function (cache) {
             return cache.addAll([
-                '/',
-                '/index.php',
-                '/assets/css/aif.min.css',
-                '/assets/js/aif.js',
-                '/assets/js/jquery.min.js',
-                '/assets/js/bootstrap.bundle.min.js',
+                'https://awpdesigns.github.io/aif/',
+                'https://awpdesigns.github.io/aif/index.html',
+                'https://awpdesigns.github.io/aif/assets/css/aif.min.css',
+                'https://awpdesigns.github.io/aif/assets/js/aif.js',
+                'https://awpdesigns.github.io/aif/assets/js/jquery.min.js',
+                'https://awpdesigns.github.io/aif/assets/js/bootstrap.bundle.min.js',
             ]);
         })
     );
